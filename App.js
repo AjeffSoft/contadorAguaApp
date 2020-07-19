b91120ceac0331c5e4fd9c3e9adc43172932de12
Jefferson Freitas <ajeffsoft@gmail.com>
2020-07-19 15:41:24 -0300

    Creating percentagem component info

--- App.js
+++ App.js
@@ -10,22 +10,27 @@ export default class ContadorAgua extends Component {
     return (
       <View style={styles.body}>
         <ImageBackground source={require('./images/waterbg.png')} style={styles.bgimage}> 
-          <View style={styles.infoArea}>
-            <View style={styles.area}>
-              <Text style={styles.areaTitulo}>Meta</Text> 
-              <Text style={styles.areaDado}>2000ml</Text> 
-            </View>
-            <View style={styles.area}>
-              <Text style={styles.areaTitulo}>Consumido</Text> 
-              <Text style={styles.areaDado}>500ml</Text> 
-            </View>
-            <View style={styles.area}>
-              <Text style={styles.areaTitulo}>Status</Text> 
-              <Text style={styles.areaDado}>Ruim</Text> 
+          <View>
+            <View style={styles.infoArea}>
+              <View style={styles.area}>
+                <Text style={styles.areaTitulo}>Meta</Text> 
+                <Text style={styles.areaDado}>2000ml</Text> 
+              </View>
+              <View style={styles.area}>
+                <Text style={styles.areaTitulo}>Consumido</Text> 
+                <Text style={styles.areaDado}>500ml</Text> 
+              </View>
+              <View style={styles.area}>
+                <Text style={styles.areaTitulo}>Status</Text> 
+                <Text style={styles.areaDado}>Ruim</Text> 
+              </View>
             </View>
 
+            <View style={styles.percArea}>
+              <Text style={styles.percText}>25%</Text>
+              
+            </View>
           </View>
-
         </ImageBackground>
       </View>
     );
@@ -46,9 +51,18 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     marginTop: 70
   },
+  percArea: {
+    marginTop: 160,
+    alignItems: 'center'
+  },
   area: {
     flex: 1,
     alignItems: 'center'
+  },
+  percText: {
+    fontSize : 75,
+    color: '#fff',
+    backgroundColor: 'transparent'
   },
   areaTitulo : {
     color: '#45b2fc'
